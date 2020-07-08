c48681acfb2de614018fca9c8697f0496243d688
itoutakeshi <take339@hotmail.com>
2020-07-08 22:18:35 +0900

    インクリメンタルサーチの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  
   devise_for :users
-  
-  root 'groups#index'   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  resources :users, only: [:edit, :update]
+  root 'groups#index'
+  # :indexを追記
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
 end
