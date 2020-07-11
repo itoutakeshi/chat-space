15b35a6f22953e849c7848acb960f529eecd0273
itoutakeshi <take339@hotmail.com>
2020-07-11 12:01:31 +0900

    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -76,58 +76,28 @@
     var reloadMessages = function() {
       var last_message_id = $('.message:last').data("message-id");
       $.ajax({
-        url: "/api/messages",
+        url: "api/messages",
         type: 'get',
         dataType: 'json',
         data: {id: last_message_id}
       })
 
     .done(function(messages) {
-      console.log('success');
+      if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      //メッセージが入ったHTMLに、入れ物ごと追加
+      $('.messages').append(insertHTML);
+    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
     })
     .fail(function() {
       alert('error');
     });
   };
-  //setInterval(reloadMessages, 7000);
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 15000);
+  }
 });
-
-
-
-//   function buildHTML(message){
-//    if ( message.image ) {
-//      var html =
-//         `<div class="message_list">
-//            <div class="message__list__info">
-//              ${message.user_name}
-//            </div>
-//            <div class="message__list__time">
-//              ${message.created_at}
-//            </div>
-//          </div>
-//          <div class="message__text">
-//            <p class="message__text__list">
-//              ${message.content}
-//            </p>
-//          </div>
-//          <img src=${message.image} >`
-//      return html;
-//    } else {
-//      var html =
-//       `<div class="message_list">
-//         <div class="message__list__info">
-//           ${message.user_name}
-//         </div>
-//         <div class="message__list__time">
-//           ${message.created_at}
-//         </div>
-//       </div>
-//       <div class="message__text">
-//         <p class="message__text__list">
-//           ${message.content}
-//         </p>
-//       </div>`
-//      return html;
-//    };
-// }
-
